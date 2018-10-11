cc8b13ac6632dabd62bcab950703da561a5bf767
zhangchq <zchunchun2777@163.com>
2018-10-11 09:38:55 +0800

    项目信息 c u

--- src/main/java/com/atlp/jzfp/service/fpxm/xmjd/XmjdServiceImpl.java
+++ src/main/java/com/atlp/jzfp/service/fpxm/xmjd/XmjdServiceImpl.java
@@ -1,46 +1,47 @@
-package com.atlp.jzfp.service.fpxm.xmxx;
+package com.atlp.jzfp.service.fpxm.xmjd;
 
 import com.atlp.jzfp.common.utils.AtlpUtil;
 import com.atlp.jzfp.entity.fpxm.JzfpBXmJdEntity;
-import com.atlp.jzfp.entity.fpxm.JzfpBXmXxEntity;
 import com.atlp.jzfp.repository.fpxm.FpxmXmjdRepository;
-import com.atlp.jzfp.repository.fpxm.FpxmXmxxRepository;
-import com.atlp.jzfp.service.fpxm.xmjd.IXmjdService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 import javax.servlet.http.HttpServletRequest;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
  * @Author: zhangchq
- * @CreateTime: 2018-10-10 16:39
+ * @CreateTime: 2018-10-10 18:29
  * @Decription:
  */
 @Slf4j
-public class XmxxServiceImpl implements IXmxxService {
+@Service
+@Transactional
+public class XmjdServiceImpl implements IXmjdService {
 
-    @Autowired
-    private FpxmXmxxRepository xmxxRepository;
     @Autowired
     private FpxmXmjdRepository xmjdRepository;
-    @Autowired
-    private IXmjdService iXmjdService;
 
     @Override
-    public Map<String, Object> doSaveOrUpdate(JzfpBXmXxEntity entity, HttpServletRequest request) throws Exception {
+    public JzfpBXmJdEntity getInfoById(String id) throws Exception {
+        return xmjdRepository.findByJdid(id);
+    }
+
+    @Override
+    public Map<String, Object> doSaveOrUpdate(JzfpBXmJdEntity entity, HttpServletRequest request) throws Exception {
         Map<String, Object> reMap = new HashMap<>();
         reMap.put("code", "0");
         reMap.put("msg", "SUCCESS");
 
         try {
-            JzfpBXmXxEntity saveEntity = new JzfpBXmXxEntity();
+            JzfpBXmJdEntity saveEntity = new JzfpBXmJdEntity();
             // 判断主键id，增加或是修改
             if (AtlpUtil.isEmpty(entity.getXmid())) {
                 BeanUtils.copyProperties(entity, saveEntity, AtlpUtil.getNullPropertyNames(entity));
@@ -50,11 +51,11 @@ public class XmxxServiceImpl implements IXmxxService {
                 saveEntity.setDqzt("有效");
             } else {
                 // 查询原项目信息是否存在
-                saveEntity = xmxxRepository.findByXmid(entity.getXmid());
+                saveEntity = xmjdRepository.findByJdid(entity.getJdid());
                 if (AtlpUtil.isEmpty(saveEntity)) {
-                    log.debug("参数异常，查询项目信息失败...项目信息id==={}", entity.getXmid());
+                    log.debug("参数异常，查询项目阶段信息失败...阶段信息id==={}", entity.getXmid());
                     reMap.put("code", "-1");
-                    reMap.put("msg", "系统异常，查询项目信息失败.");
+                    reMap.put("msg", "系统异常，查询项目阶段信息失败.");
                     return reMap;
                 }
                 BeanUtils.copyProperties(entity, saveEntity, AtlpUtil.getNullPropertyNames(entity));
@@ -62,29 +63,38 @@ public class XmxxServiceImpl implements IXmxxService {
             }
 
             // 数据保存
-            JzfpBXmXxEntity save = xmxxRepository.save(saveEntity);
-            if (null == save || null == save.getXmid()) {
-                log.debug("添加或修改项目信息失败...原项目信息信息==={}，新项目信息信息==={}",
+            JzfpBXmJdEntity save = xmjdRepository.save(saveEntity);
+            if (null == save || null == save.getJdid()) {
+                log.debug("添加或修改项目阶段信息失败...原项目阶段信息==={}，新项目阶段信息==={}",
                         saveEntity.toString(), save.toString());
                 reMap.put("code", "-2");
-                reMap.put("msg", "系统异常，维护项目信息失败.");
+                reMap.put("msg", "系统异常，维护项目阶段信息失败.");
                 return reMap;
-            }
-
-            // 项目阶段
-            List<JzfpBXmJdEntity> entityList = entity.getXmJdEntityList();
-            for (JzfpBXmJdEntity xmJdEntity : entityList) {
-                // 项目阶段主键id是否存在
-                if (AtlpUtil.isEmpty(xmJdEntity.getJdid())) {
-                    // 新添加项目阶段，项目id数据填充
-                    xmJdEntity.setXmid(save.getXmid());
-                }
-                iXmjdService.doSaveOrUpdate(xmJdEntity, request);
             }
         } catch (Exception e) {
             e.printStackTrace();
             TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
             throw new Exception();
+        }
+
+        return reMap;
+    }
+
+    @Override
+    public Map<String, Object> doDelete(JzfpBXmJdEntity entity) throws Exception {
+        Map<String, Object> reMap = new HashMap<>();
+        reMap.put("code", "0");
+        reMap.put("msg", "SUCCESS");
+
+        try {
+            xmjdRepository.delete(entity);
+        } catch (Exception e) {
+            log.debug("删除项目阶段失败...项目阶段信息==={}", entity.toString());
+            e.printStackTrace();
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            reMap.put("code", "-1");
+            reMap.put("msg", "删除项目阶段失败.");
+            return reMap;
         }
 
         return reMap;
