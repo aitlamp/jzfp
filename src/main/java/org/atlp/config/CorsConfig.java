5fa0c1a74a6cd3fe73678cb28d84bd2054a346da
caotch <441799574@qq.com>
2018-10-19 17:36:05 +0800

    跨域问题

---
+++ src/main/java/org/atlp/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package org.atlp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域配置
+ *
+ * @author 曹铁诚
+ * @since 2018年10月19日 11:46:22
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
+                .maxAge(3600);
+    }
+}
